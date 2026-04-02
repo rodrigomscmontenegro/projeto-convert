70c7a38b51d7f7abbdad3bea800d552ab61de631
rodrigomscmontenegro <rodrigodecastromontenegro@gmail.com>
2026-04-02 17:10:17 -0300

    obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amount para aceitar somente números.
 amount.addEventListener("input", () => {
     const hasCharacterRegex = /\D+/g
     amount.value = amount.value.replace(hasCharacterRegex, "")
 })
+
+// Capturando o evento de submit do formulário.
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+}
