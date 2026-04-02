9bf2903c1b19af7b2839d0bb5a43b99c34bd1a74
rodrigomscmontenegro <rodrigodecastromontenegro@gmail.com>
2026-04-02 17:53:15 -0300

    aplicando o calculo do total

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para aceitar somente números.
 amount.addEventListener("input", () => {
@@ -39,6 +40,12 @@ function convertCurrency(amount, price, symbol) {
        // Exibindo a cotação da moeda selecionada.
         description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}` 
        
+        // Calcula o total
+        let total = amount * price
+
+        // Exibir o resultado total.
+        result.textContent = total
+
         // Aplica a classe que exibe o footer para mostra o resultado.
         footer.classList.add("show-result")
     } catch (error) {
