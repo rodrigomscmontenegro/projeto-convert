78465c7c67c3f2e1ed9d53fad93a7028d847d7c6
rodrigomscmontenegro <rodrigodecastromontenegro@gmail.com>
2026-04-02 16:47:52 -0300

    manipulando o input para receber só números

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const amount = document.getElementById("amount")
 
-// Manipulando o input amount para receber somente números.
+// Manipulando o input amount para aceitar somente números.
 amount.addEventListener("input", () => {
-    console.log(amount.value)
+    const hasCharacterRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharacterRegex, "")
 })
